9bf4cb16c7927110040740a2bf00f5e5f4ec35aa
hugo-paulo <47899992+hugo-paulo@users.noreply.github.com>
2023-10-06 16:04:41 +0100

    Add files via upload

---
+++ WeatherForecastController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeaderRequestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
+        {
+        //For all headers
+        //Dictionary<string, string> requestHeaders = new Dictionary<string, string>();
+        //foreach (var header in Request.Headers)
+        //{
+        //    requestHeaders.Add(header.Key, header.Value);
+        //}
+
+        //https://www.infoworld.com/article/3617984/how-to-read-request-headers-in-aspnet-core-5-mvc.html#:~:text=Using%20the%20%5BFromQuery%5D%20and%20%5B,your%20controller%20using%20request%20headers.
+
+            Request.Headers.TryGetValue("country", out var headerValue);
+
+            string? country = headerValue.FirstOrDefault();
+
+            if (country != null)
+            {
+                if (country == "Italy")
+                {
+                    return Ok(country);
+                }
+                else
+                {
+                    return BadRequest("Incorrect Value for country");
+                }
+            }
+
+            var res = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return Ok(res);
+        }
+    }
+}
